7324fa3397001cacaa048d746375ccdffb3764de
vuy8huyhoang <vuy8huyhoang@gmail.com>
2024-12-06 09:48:53 +0700

    ss

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: ['tailwindui.com'], 
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.vuai.vn',
+        port: '',
+        pathname: '/img/**',
+      },
+    ],
   },
 };
 
