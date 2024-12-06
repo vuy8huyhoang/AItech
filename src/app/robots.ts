cd32a0d8f10ef312f3229690b7fba43c069371bf
vuy8huyhoang <vuy8huyhoang@gmail.com>
2024-12-06 08:13:53 +0700

    sss

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,13 +1,16 @@
 import type { MetadataRoute } from 'next'
-const SITE_URL = process.env.SITEMAP_URL || 'https://acme.com/sitemap.xml';
+
+const SITE_URL = process.env.NEXT_PUBLIC_SITEMAP_URL || 'https://acme.com/sitemap.xml';
 
 export default function robots(): MetadataRoute.Robots {
     return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-            disallow: ['/private/','/admin'],
-        },
+        rules: [
+            {
+                userAgent: '*',
+                allow: '/',
+                disallow: ['/private/', '/admin'],
+            }
+        ],
         sitemap: SITE_URL,
     }
 }
