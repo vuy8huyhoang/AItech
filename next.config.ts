6a8abd1b8388f99b2d80a2d65a226665d26d44c1
vuy8huyhoang <vuy8huyhoang@gmail.com>
2024-12-06 10:01:52 +0700

    ss

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: ['tailwindui.com'], 
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'www.vuai.vn',
-        port: '',
-        pathname: '/assets/img/**',
-      },
-    ],
+  
   },
 };
 
